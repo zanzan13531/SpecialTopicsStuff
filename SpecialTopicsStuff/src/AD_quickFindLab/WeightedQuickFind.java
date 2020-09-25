9c85becf9a11ddf0464ba3fd971edeb04921ea94
zanzan13531 <zanzan13531@gmail.com>
2020-09-25 10:22:44 -0700

    forgot to add weighedunionfind

---
+++ SpecialTopicsStuff/src/AD_quickFindLab/WeightedQuickFind.java
@@ -0,0 +1,49 @@
+package AD_quickFindLab;
+
+public class WeightedQuickFind implements UnionFind {
+	
+	private int[] thingy;
+	private int sizey;
+
+	public WeightedQuickFind(int n) {
+		
+		thingy = new int[n];
+		sizey = n;
+		
+		for (int i = 0; i < sizey; i++) {
+			thingy[i] = i;
+		}
+		
+	}
+	
+	public void Union(int a, int b) {
+		
+		thingy[Find(a)] = Find(b);
+		
+	}
+	
+	public int Find(int a) {
+		
+		if (thingy[a] == a) {
+			
+			return (a);
+		
+		} else {
+			
+			thingy[a] = Find(thingy[a]);
+			return (Find(thingy[a]));
+			
+		}
+		
+	}
+	
+	public String toString() {
+		String s = "";
+		for (int i = 0; i < sizey; i++) {
+			s += thingy[i];
+		}
+		
+		return (s);
+	}
+	
+}
